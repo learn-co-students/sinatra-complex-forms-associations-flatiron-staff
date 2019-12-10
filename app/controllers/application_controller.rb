e0c77030a3f7bcdbdab7416e4edba85c5f4b8bd7
Rupinder Dhillon <rupinder.dhillon@flatironschool.com>
2019-12-10 23:57:49 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,37 @@ class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
+
+  patch '/owners/:id' do
+    if !params[:owner].keys.include?("pet_ids")
+      params[:owner]["pet_ids"] = []
+    end
+
+    @owner = Owner.find(params[:id])
+    @owner.name = params["owner"]["name"]
+    @owner.pet_ids = params["owner"]["pet_ids"]
+
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+
+    @owner.save
+
+    redirect "owners/#{@owner.id}"
+  end
+
+  patch '/pets/:id' do
+    @pet = Pet.find(params[:id])
+
+    if params[:owner_name].length > 0
+      owner = Owner.create!(name: params[:owner_name])
+    else
+      owner = Owner.find(params[:pet_owner])
+    end
+
+    @pet.update!(name: params[:pet_name], owner_id: owner)
+    owner.pets << @pet unless owner.pets.include?(@pet)
+
+    redirect to "pets/#{@pet.id}"
+  end
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,24 +2,51 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
+
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
+    if params[:owner_name].length > 0
+      owner = Owner.create(name: params[:owner_name])
+    else
+      owner = Owner.find(params[:pet_owner])
+    end
+
+    @pet = Pet.create!(owner: owner, name: params[:pet_name])
+    owner.pets << @pet
 
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+
+    erb :'/pets/edit'
+  end
+
+  patch '/pets/:id' do
+    @pet = Pet.find(params[:id])
+
+    if params[:owner][:name].length > 0
+      owner = Owner.create!(name: params[:owner][:name])
+    else
+      owner = Owner.find(params[:pet_owner])
+    end
+
+    @pet.update!(name: params[:pet_name], owner_id: owner)
+    owner.pets << @pet unless owner.pets.include?(@pet)
 
     redirect to "pets/#{@pet.id}"
   end
