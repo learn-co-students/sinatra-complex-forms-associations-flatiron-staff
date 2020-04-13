79bd9c9b1572db5e3464987e0624bbdd7ba69768
github username <github email address>
2020-04-13 18:35:56 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,11 +6,21 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    # binding.pry
+    @pet = Pet.create(name: params[:pet_name])
+    if params[:owner_id]
+      @pet.owner = Owner.find(params[:owner_id])
+      @pet.save
+    end
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+      @pet.save
+    end
     redirect to "pets/#{@pet.id}"
   end
 
@@ -19,8 +29,22 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do 
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
 
+  post '/pets/:id' do 
+    # binding.pry
+    @pet = Pet.find(params[:id])
+    @pet.update(:name => params["pet_name"])
+    @pet.owner = Owner.find(params["owner_id"])
+    @pet.save
+    if !params["owner_name"].empty?
+      @pet.owner = Owner.create(name: params["owner_name"])
+      @pet.save
+    end
     redirect to "pets/#{@pet.id}"
   end
 end
